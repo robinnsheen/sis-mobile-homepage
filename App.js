866a5e535938cf10580f8f177ca7c305f2c731c3
robin <robinnsheen@berkeley.edu>
2022-08-16 11:43:42 -0700

    Wrote api call to get assessmentsessions list and set state

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import axios from "axios";
+import SISapi from "./api";
 
 
 
@@ -9,44 +10,19 @@ import axios from "axios";
 
 export default function App() {
 
-  const [token, setToken] = useState("");
+  const [assessmentSessions, setAssessmentSessions] = useState("");
 
   useEffect(function damnitBrian() {
 
-    async function hiBrian() {
+    const res = SISapi.getAssessmentSessionsList();
+    console.log(res);
+    setAssessmentSessions(res);
+  }, [assessmentSessions]);
 
-      // const res = await axios({
-      //   url: "http://localhost:8000/api/-token/",
-      //   method: "post",
-      //   data: {
-      //     "username": "admin",
-      //     "password": "password"
-      //   }
-      // })
-      // console.log(res);
-      // setToken(res.data.token);
-      const res = await axios({
-        url: "http://localhost:8000/api/assessmentsessions/",
-        method: "get",
-        headers: {
-          "Authorization": "Token 71ba9fbb567c76e604ac05dd28ec8a3bfea61073",
-          "Content-Type": "application/json",
-          // "Accept": "*/*"
-          // "Cookie": "csrftoken=CTWACQNDXEwO2DEdQpUCXzvLpVhBr5rxxDQVRFWZxEfd8VngjR92g6XAQLRs6Q0a"
-        }
-      })
-      // console.log(res.data);
-      // setToken(res.data.results);
-    }
-    hiBrian();
-    // const token = hiBrian();
-    // setToken(token);
-  }, []);
-  
 
   return (
     <View style={styles.container}>
-      <Text>{ token.title }</Text>
+      <Text>{ assessmentSessions[0].title }</Text>
       <StatusBar style="auto" />
     </View>
   );
