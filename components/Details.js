5d44d9ed91173ad47e0aad55c84284a36705eb04
robin <robinnsheen@berkeley.edu>
2022-08-18 14:12:53 -0700

    Styling and formating details component

--- components/Details.js
+++ components/Details.js
@@ -7,11 +7,44 @@ function Details({ items }) {
   const { id, type } = route.params;
   const item = items.filter(i => i.id === id && i.type == type)[0];
 
+  const types = {
+    "V": "Event",
+    "E": "Exercise",
+    "L": "Lecture",
+    "A": "Assessment"
+  };
+
+  const date = new Date(item.start_date).toLocaleDateString(
+    'en-us',
+    {
+      weekday: "short",
+      day: "numeric",
+      month: "numeric",
+      year: "numeric",
+    });
+
+  const startTime = new Date(item.start_at).toLocaleTimeString(
+    'en-us',
+    {
+      hour: "numeric",
+      minute: "numeric"
+    }
+  );
+
   return (
     <SafeAreaView style={styles.container}>
-      <Text>{item.title}</Text>
-      <Text>id: {id}</Text>
-      <Text>type: {type}</Text>
+      <View>
+        <Text>{item.title}</Text>
+        <Text style={styles.type}>{item.type}</Text>
+        <Text>{item.description}</Text>
+        <Text>{item.date}, {item.startTime}</Text>
+      </View>
+
+      <View>
+        <Text>Staff:</Text>
+        {items.staff_ids.map(s => <Text>{s}</Text>)}
+      </View>
+
     </SafeAreaView>
   );
 }
@@ -20,7 +53,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-  }
+    fontFamily: 'Georgia',
+  },
+  type: {
+    color: "darkgray",
+  },
 });
 
 
